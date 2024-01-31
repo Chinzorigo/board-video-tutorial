dd4fca00d76368b1b4dcf2796671ba3795a81978
Chinzorigo <chinzorig.o@gmail.com>
2024-01-31 22:59:16 +0800

    Delete unused files and add convex package

--- app/_test/subroute/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div>Rendered at (test)/subroute</div>;
-};
-
-export default Page;
--- app/users/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>User List!</div>;
-};
-
-export default Page;
